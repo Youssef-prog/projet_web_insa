f58fc088dce64a4f85ca211aa6dad343d81d2edf
M. Sanchez Jean-Christophe <jcsanche@insa-20530>
2019-12-13 10:21:25 +0100

    remove all files

--- contact/js/contact.js
+++
@@ -1,9 +0,0 @@
-function check(){
-    fname = document.getElementById("first_name").value;
-    lname = document.getElementById("last_name").value;
-    email = document.getElementById("email").value;
-
-    if(fname === "" || lname === "" || email === ""){
-        window.alert("please fill all the form");
-    }
-}
